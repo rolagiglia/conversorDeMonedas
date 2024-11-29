55291c226aa57c265dc00679c2b1dff372a00f17
rolagiglia <142903251+rolagiglia@users.noreply.github.com>
2024-11-29 16:32:39 -0300

    Add files via upload
    Modifico API utilizada por una gratuita. Funcional

--- script.js
+++ script.js
@@ -11,22 +11,42 @@ document.getElementById("conversionForm").addEventListener("submit", async (e) =
         return;
     }
 
-    const API_KEY = "73674022f35423a472806e7a59154c7a"; // Reemplaza con tu clave real
-    const BASE_URL = "http://api.exchangeratesapi.io/v1/lastest";
+   const BASE_URL = "https://Exchange-Rate.proxy-production.allthingsdev.co/ExchangeRate";
+    
+    const myHeaders = new Headers();
+    // mi api key
+    myHeaders.append("x-apihub-key", "EBINIHzN-5HGei39yhBC6ZkhCUrtrEhCsRH3B3mC-s5-GXVzsM");
+    myHeaders.append("x-apihub-host", "Exchange-Rate.allthingsdev.co");
+    myHeaders.append("x-apihub-endpoint", "c9207840-b078-4940-be6f-ffb3034ac644");
 
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+     };
     try {
-        const url = `${BASE_URL}?access_key=${API_KEY}&symbols=${monedaDestino}`;  
-        const response = await fetch(url);      
-        if (!response.ok) throw new Error("No se pudo obtener la tasa de cambio.");
-        const data = await response.json();
-        const tasa = data.rates[monedaDestino];
-        const resultado = valor * tasa;
-
-        document.getElementById("resultado").innerText =
-            `El resultado es ${resultado.toFixed(2)} ${monedaDestino}`;
-    } catch (error) {
-        document.getElementById("resultado").innerText =
-             `Ocurrió un error al realizar la conversión. Inténtalo más tarde. ${tasa}`;
-        console.error(error);
-    }
+          // Construye la URL de la solicitud
+            const url = `${BASE_URL}?fromCurrency=${monedaOrigen}&toCurrency=${monedaDestino}&amount=${valor}`;
+            const response = await fetch(url,requestOptions);
+            
+    
+            // Verifica si la respuesta es válida
+            if (!response.ok) throw new Error("No se pudo obtener la tasa de cambio.");
+            
+            const data = await response.json();
+                
+            // Obtiene el resultado
+            const resultado = data.convertedAmount;
+            if (!resultado) throw new Error(`No se encontró la tasa para ${monedaDestino}.`);
+    
+            // Muestra el resultado en la página
+            document.getElementById("resultado").innerText =
+                `El resultado es ${resultado.toFixed(2)} ${data.toCurrency}`;
+      
+                } catch (error) {
+            // Maneja errores y los muestra al usuario
+            document.getElementById("resultado").innerText =
+                "Ocurrió un error al realizar la conversión. Inténtalo más tarde.";
+            console.error(error.message);
+        }
 });
